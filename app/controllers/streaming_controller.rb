0ec10ef5afa14cd12196ee6151b964a5b35e8e18
Hunter Braun <hunter.braun@gmail.com>
2019-05-14 17:11:10 -0500

    Add streaming endpoint

---
+++ app/controllers/streaming_controller.rb
@@ -0,0 +1,13 @@
+class StreamingController < ApplicationController
+  include ActionController::Live
+
+  def index
+    response.headers['Content-Type'] = 'text/event-stream'
+    100.times {
+      response.stream.write "hello world\n"
+      sleep 1
+    }
+  ensure
+    response.stream.close
+  end
+end
